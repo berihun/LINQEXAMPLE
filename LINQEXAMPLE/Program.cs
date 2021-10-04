3c36bf73643d80700f8891d2f3a4d16a3d5926a0
Berihun Hadis <berihunhadis@gmail.com>
2021-10-04 08:56:20 +0300

    first commit

---
+++ LINQEXAMPLE/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Linq;
+
+namespace LINQEXAMPLE
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //our data source
+            string[] names = { "Billy", "John", "Steve", "Brown" };
+            //linq query
+            var myLinqResult = from name in names where name.Contains('B') select name;
+            //execute query
+            Console.WriteLine("++++++++++++++++++++++++++++++++++++++++++++++++");
+            Console.WriteLine("we are strating to show linq result: ...");
+            foreach (var name in myLinqResult)
+                Console.WriteLine(name);
+            Console.WriteLine("++++++++++++++++++++++++++++++++++++++++++++++++");
+        }
+    }
+}
